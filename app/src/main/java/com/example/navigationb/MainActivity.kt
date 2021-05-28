0b9c0e143f12fea893d6ffcde5dc97079928153f
Giulio <caporrogiulio@gmail.com>
2021-05-28 13:12:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navigationb/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.navigationb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigationb.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var binding: ActivityMainBinding
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment,R.id.secondFragment, R.id.thirdFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        bottomNavigationView.isVisible = false
+
+    }
+}
---
+++ app/src/main/java/com/example/navigationb/SplashFragment.kt
@@ -0,0 +1,33 @@
+package com.example.navigationb
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+
+        // Inflate the layout for this fragment
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splash_Fragment_to_firstFragment)
+
+        }, 3000)
+        return inflater.inflate(R.layout.fragment_splash_, container, false)
+    }
+
+}
+
