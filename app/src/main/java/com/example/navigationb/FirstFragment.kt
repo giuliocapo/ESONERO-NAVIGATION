2a13203a6805b0576359536347643f930f90a8e8
Giulio <caporrogiulio@gmail.com>
2021-05-30 16:35:43 +0200

    Classes names and launcher icon

--- app/src/main/java/com/example/navigationb/FirstFragment.kt
+++ app/src/main/java/com/example/navigationb/FirstFragment.kt
@@ -1,12 +1,16 @@
 package com.example.navigationb
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.constraintlayout.solver.widgets.ConstraintWidget.VISIBLE
+import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
 import androidx.core.view.isVisible
 import androidx.navigation.Navigation
 import com.example.navigationb.MainActivity
@@ -21,6 +25,10 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //image
+        //ImageView.inflate(R.drawable.run1)
+
         binding.bottomNavigationView.isVisible = true
 
         val viewFirst =inflater.inflate(R.layout.fragment_first, container, false)
